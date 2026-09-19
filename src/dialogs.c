ee4413306c3ca49bbb4601a8119daa004e181dbd
NathanNeurotic (Ripto) <noahtkevans@gmail.com>
2026-09-19 05:06:42 -0700

    fix(ui): keep item text above disc art and add disc toggle

--- src/dialogs.c
+++ src/dialogs.c
@@ -750,6 +750,11 @@ struct UIItem diaArtworkConfig[] = {
     {UI_BOOL, UICFG_COVERART, 1, 1, -1, 0, 0, {.intvalue = {0, 0}}},
     {UI_BREAK},
 
+    {UI_LABEL, 0, 1, 1, -1, -40, 0, {.label = {"Disc Artwork", -1}}},
+    {UI_SPACER},
+    {UI_BOOL, UICFG_ENABLE_DISCART, 1, 1, -1, 0, 0, {.intvalue = {0, 0}}},
+    {UI_BREAK},
+
     {UI_LABEL, 0, 1, 1, -1, -40, 0, {.label = {"Background Art", -1}}},
     {UI_SPACER},
     {UI_BOOL, UICFG_ENABLE_BGART, 1, 1, -1, 0, 0, {.intvalue = {0, 0}}},
