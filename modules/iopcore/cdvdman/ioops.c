a523c4b78e7e929cc8678416393aba515725db1c
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-09-20 12:22:08 -0700

    chore: align cdvdman formatting with rebuild (ChatGPT/OpenAI)

--- modules/iopcore/cdvdman/ioops.c
+++ modules/iopcore/cdvdman/ioops.c
@@ -238,7 +238,6 @@ static int cdrom_open(iop_file_t *f, const char *filename, int mode)
         return -ENAMETOOLONG;
 
     DPRINTF("cdrom_open %s mode=%d layer %d\n", filename, mode, f->unit);
-
     strncpy(path_buffer, filename, sizeof(path_buffer) - 1);
     path_buffer[sizeof(path_buffer) - 1] = '\0';
 
@@ -382,7 +381,6 @@ static int cdrom_getstat(iop_file_t *f, const char *filename, iox_stat_t *stat)
         return -ENAMETOOLONG;
 
     DPRINTF("cdrom_getstat %s layer %d\n", filename, f->unit);
-
     strncpy(path_buffer, filename, sizeof(path_buffer) - 1);
     path_buffer[sizeof(path_buffer) - 1] = '\0';
 
